e43112776021120fcbed581cc3b1b6cddd80262c
Nazikallaberdiyeva <nazikallaberdiyeva@gmail.com>
2020-11-10 15:24:53 -0600

    intial commit

--- src/hello.java
+++ src/hello.java
@@ -2,10 +2,7 @@ public class hello {
     public static void main(String[] args) {
 
         System.out.println("Hello");
-        // line 1
-        // line 2
-        // line 3
-
+       // line 1
 
 
 
